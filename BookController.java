884f45d8d1ca9eca6726a1da132c29d4c931dd1d
Anne Apiyo <anneapiyo0@gmail.com>
2024-03-22 14:50:50 +0300

    Add files via upload

--- BookController.java
+++ BookController.java
@@ -9,20 +9,21 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
     @Autowired
     private BookRepo bookRepo;
 
-    @GetMapping
+    @GetMapping("/getAllBooks")
     public ResponseEntity<List<Book>> getAllBooks() {
      try {
          List<Book>bookList=new ArrayList<>();
          bookRepo.findAll().forEach(bookList::add);
 
          if (bookList.isEmpty()){
-             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+             return new ResponseEntity<>(bookList, HttpStatus.NO_CONTENT);
          }
         return new ResponseEntity<>(HttpStatus.OK);
      }catch(Exception ex){
@@ -32,19 +33,39 @@ public class BookController {
     }
 
     @GetMapping("/getBookById/{id}")
-    public void getBookById(@PathVariable Long id){
+    public ResponseEntity<Book> getBookById(@PathVariable Long id){
+        Optional<Book> bookData= bookRepo.findById(id);
 
+        if (bookData.isPresent()){
+            return  new ResponseEntity<>(bookData.get(),HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-    @PostMapping
-  public void addBook(){
-
+    @PostMapping("/addBook")
+  public ResponseEntity<Book> addBook(@RequestBody Book  book){
+       Book bookObj= bookRepo.save(book);
+     return new ResponseEntity<>(bookObj,HttpStatus.OK);
   }
-  @PostMapping
- public void updateBookById(){
+  @PostMapping("/updateBookById{id}")
+ public ResponseEntity<Book> updateBookById(@PathVariable Long id, @RequestBody Book newBookData){
+   Optional<Book> oldBookData=bookRepo.findById(id);
+
+   if (oldBookData.isPresent()){
+      Book updateBookData=oldBookData.get();
+      updateBookData.setTitle(newBookData.getTitle());
+      updateBookData.setAuthor(newBookData.getAuthor());
+
+     Book bookObj= bookRepo.save(updateBookData);
+     return new ResponseEntity<>(bookObj,HttpStatus.OK);
+
+   }
+   return  new ResponseEntity<>(HttpStatus.NOT_FOUND);
 
  }
-@DeleteMapping
- public void deleteBookById(){
+@DeleteMapping("/deleteBookById{id}")
+ public  ResponseEntity<HttpStatus> deleteBookById(@PathVariable Long id){
+        bookRepo.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
 
  }
 }
